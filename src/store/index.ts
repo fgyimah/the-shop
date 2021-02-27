02436ec7cf21dd0490d3ab8524917c669bda76cd
Francis Gyimah <fgyimah883@gmail.com>
2021-02-27 18:31:36 +0000

    Added redux persist to persist cart state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,26 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from './root';
 
 const middlewares = [thunk];
 
 let store: any;
 
+const persistConfig = {
+	key: 'tendoSHOP-root',
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 if (process.env.NODE_ENV === 'development') {
-	store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+	store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 } else {
-	store = createStore(rootReducer, applyMiddleware(...middlewares));
+	store = createStore(persistedReducer, applyMiddleware(...middlewares));
 }
 
+export const persistor = persistStore(store);
 export default store;
